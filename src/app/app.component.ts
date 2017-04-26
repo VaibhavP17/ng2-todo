e5611e77c7afd165fdb997b2ec0a1d54c3f3ea24
pachauri <vaiby1111@yahoo.co.in>
2017-04-26 22:52:09 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'todos',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  newTodo: string;
+    todos: any;
+    todoObj: any;
+
+    constructor() {
+      this.newTodo = '';
+      this.todos = [];
+    }
+
+    addTodo(event) {
+      this.todoObj = {
+        newTodo: this.newTodo,
+        completed: false
+      }
+      this.todos.push(this.todoObj);
+      this.newTodo = '';
+      event.preventDefault();
+    }
+
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+
+    deleteSelectedTodos() {
+      //need ES5 to reverse loop in order to splice by index
+      for(var i=(this.todos.length -1); i > -1; i--) {
+        if(this.todos[i].completed) {
+          this.todos.splice(i, 1);
+        }
+      }
+    }
+
 }
