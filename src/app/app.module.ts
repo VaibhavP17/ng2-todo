05b10d2aba999253e512a54b43cefbad67a671bb
pachauri <vaiby1111@yahoo.co.in>
2017-04-28 00:00:26 +0530

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { MyTodoComponent } from './my-todo/my-todo.component';
+import { MyTestComponent } from './components/my-test/my-test.component';
+
+
+const appRoutes : Routes = [
+  {
+    path: 'test', component: MyTestComponent
+  },
+  {
+    path: '',
+    redirectTo: '/test',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyTodoComponent
+    MyTodoComponent,
+    MyTestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
